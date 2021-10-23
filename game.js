c7ae166ad85b9c654104994ee629a180a08019ec
Simon Dahlgren <sida2000@student.miun.se>
2021-10-23 21:37:51 +0200

    added level and items

--- game.js
+++ game.js
@@ -6,8 +6,14 @@ debug: true,
 clearColor: [ 0, 0, 0, 1],
 })
 
+const ENEMY_SPEED = 30
 const MOVE_SPEED  = 120
 const JUMP_FORCE = 360
+let CURRENT_JUMP_FORCE = JUMP_FORCE
+const BIG_JUMP_FORCE = 550
+let isJumping = true
+const FALLDEATH = 350
+
 
 loadRoot('https://i.imgur.com/')
 loadSprite('coin','wbKxhcd.png')
@@ -23,19 +29,20 @@ loadSprite('pipe-top-right','hj2GK4n.png')
 loadSprite('pipe-bottom-left','c1cYSbt.png')
 loadSprite('pipe-bottom-right','nqQ79eI.png')
 
-scene("game", () => {
+scene("game", ({level, score}) => {
 layers(['bg','obj','ui'], 'obj')
 
 const map = [
-    '                                       ',
-    '                                       ',
-    '                                       ',
-    '                                       ',
-    '    %   =*=%=                          ',
-    '                                       ',
-    '                           -+          ',
-    '                 ^  ^      ()           ',
-    '============================= =========',
+    '                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ',
+    '                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ',
+    '                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ',
+    '                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ',
+    '                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ',
+    '                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ',
+    '                                       =================================            =======================                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               ',
+    '        %  *                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ',
+    '                          ^     ^   ^     ^  ^                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ',
+    '============================= ================================================================================   ===========================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================   =============================================================================',
     
 ]
 
@@ -43,31 +50,32 @@ const levelCfg = {
     width: 20,
     height: 20,
     '=': [sprite('block'), solid()],
-    '$': [sprite('coin')],
+    '$': [sprite('coin'), "coin"],
     '%':[sprite('surprise'), solid(), 'coin-surprise'],
     '*':[sprite('surprise'), solid(), 'mushroom-surprise'],
     '}':[sprite('unboxed', solid())],
     '(':[sprite('pipe-bottom-left'), solid(), scale(0.5)],
     ')':[sprite('pipe-bottom-right'), solid(), scale(0.5)],
-    '-':[sprite('pipe-top-left'), solid(), scale(0.5)],
-    '+':[sprite('pipe-top-right'), solid(), scale(0.5)],
-    '^': [sprite('evil-shroom'), solid()],
-    '#': [sprite('mushroom'), solid()],
+    '-':[sprite('pipe-top-left'), solid(), scale(0.5)  , "pipe"],
+    '+':[sprite('pipe-top-right'), solid(), scale(0.5)  , "pipe"],
+    '^': [sprite('evil-shroom'), solid(), "dangerous"],
+    '#': [sprite('mushroom'), solid(), "mushroom", body()]
 
 }
 const gameLevel = addLevel(map, levelCfg)
-
+let points
 const scoreLabel = add([
-    text("test"),
+   
+    text(score),
     pos (30,6),
     layer("ui"),
     {
-        value: "test",
+        value: score,
     }
 ])
 
 
-add([text("level " + "test", pos(4,6))])
+add([text("level " + parseInt(level +1)), pos(60,6)])
 
 function big() {
 let timer = 0
@@ -83,7 +91,19 @@ return {
     },
     isBig() {
         return isBig
-    }
+    },
+    smallify() {
+        CURRENT_JUMP_FORCE = JUMP_FORCE
+        this.scale = vec2(1)
+        timer = 0
+        isBig = false
+    },
+    biggify(time) {
+        this.scale = vec2(2)
+        CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+        timer = time
+        isBig = false
+    },
 }
 
 }
@@ -94,10 +114,77 @@ sprite('mario'),
 solid(),
 pos(30, 0), 
 body(), 
+big(),
 origin('bot'),
 scale(1),
 
 ])
+
+
+
+player.on("headbump", (obj) => {
+    if(obj.is('coin-surprise')) {
+        gameLevel.spawn('$', obj.gridPos.sub(0, 1))
+        destroy(obj)
+        gameLevel.spawn('}', obj.gridPos.sub(0,0))
+    }
+    if(obj.is('mushroom-surprise')) {
+        gameLevel.spawn('#', obj.gridPos.sub(0, 1))
+        destroy(obj)
+        gameLevel.spawn('}', obj.gridPos.sub(0,0))
+    }
+    
+})
+action("mushroom", (m) => {
+    m.move(25,0)  
+  })
+  
+player.collides("pipe", () => {
+    keyDown("down", () => {
+      go("game", { 
+    level: (level + 1),     
+     score: scoreLabel.value,
+        })
+     
+    })
+})
+
+  player.collides("mushroom", (m) => {
+      destroy(m)
+      player.biggify(10)
+  })
+
+  player.collides("dangerous", (d) => {
+   if(isJumping)
+   {
+       destroy(d)
+   }   
+   else {  
+go("lose", {score: scoreLabel.value}
+)}
+
+})
+
+player.action(() => {
+camPos(player.pos)
+if(player.pos.y >= FALLDEATH)
+{
+go("lose", {score: scoreLabel.value}
+)
+}
+
+})
+
+  action("dangerous", (d) => {
+      d.move(-ENEMY_SPEED, 0)
+  })
+  
+  player.collides("coin", (c) => {
+      destroy(c)
+      scoreLabel.value += 1
+      scoreLabel.text += scoreLabel.value
+    })
+
 
 keyDown("left", ()=>{
     player.move(-MOVE_SPEED, 0)
@@ -105,14 +192,27 @@ keyDown("left", ()=>{
 keyDown("right", ()=>{
     player.move(MOVE_SPEED, 0)
 })
+
+player.action(() => {
+    if(player.grounded()) {
+    isJumping= false
+    }
+})
+
+
+
 keyPress("space", ()=>{
     if(player.grounded())
-    {
+    {   isJumping = true
         player.jump(JUMP_FORCE)
     }
 
-})
+})})
+
+scene("lose", ({ score }) => {
+add ([text(score, 32), origin("center"), pos(width()/2, height()/2)])
 })
 
+start("game", {level: 0, score: 0})
 
-start("game")
+
